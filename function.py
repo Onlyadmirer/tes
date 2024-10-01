b259f80a90eff47bb75bfdf96a51047e28a8badb
AkmalHstl <akmalrbc6@gmail.com>
2024-10-02 01:13:28 +0800

    kupul

---
+++ function.py
@@ -0,0 +1,53 @@
+# def makan(a, b):
+#     c = a + b
+#     return "nasi goreng " * c
+# x =  makan(2, 3)
+# print(x)
+
+
+# def penjumlahan(*a, **b):
+#     for x,y in b.items():
+#         print(x,y)
+#     for i in a:
+#         print(i)
+        
+# penjumlahan(1,2,3,tugas = 1, kuis = 2)  
+
+
+
+# def menghitung(*a):
+#     maximum = max(a)
+#     minimum  = min(a)
+#     b = sum(a)
+#     mean = b/len(a)
+#     return maximum, minimum, mean
+
+# max, min, mean = menghitung(65,98,75,88)
+# print(max)
+# print(min)
+# print(mean)
+
+# def show_intervals(start, end):
+#     print(start, end=" ")
+#     start = start + 1
+#     if start <= end:
+#         show_intervals(start, end)
+
+# show_intervals(1, 10)
+
+
+# def faktorial(n):
+#     if n == 1:
+#         return 1
+#     else:
+#       return n * faktorial(n-1)
+# result = faktorial(5)
+# print(result)
+
+
+# def perhitungan(a : float, b :int) :
+#     div = a/b
+#     return div
+# result = perhitungan(10, 2)
+# print(result)
+
